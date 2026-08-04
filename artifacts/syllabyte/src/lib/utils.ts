d377ecc559580057c3720f4d5f4136789fde4dfc
Princuu <princuu29@gmail.com>
2026-08-04 20:16:34 +0530

    first commit

--- artifacts/syllabyte/src/lib/utils.ts
+++ artifacts/syllabyte/src/lib/utils.ts
@@ -1,7 +1,4 @@
-import { twMerge } from 'tailwind-merge';
-
-import { clsx, type ClassValue } from 'clsx';
-import { ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
